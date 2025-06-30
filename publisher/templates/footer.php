fc0da53881146fb1f011f71dc7d4642884b942ce
simoncode12 <123048201+simoncode12@users.noreply.github.com>
2025-06-30 14:12:48 +0700

    Delete publisher directory

--- publisher/templates/footer.php
+++
@@ -1,6 +0,0 @@
-<?php
-// File: /publisher/templates/footer.php (NEW)
-?>
-    </div> <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
